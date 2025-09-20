c07d8c1a1e3a84ad3e0220db2eb92c14a8af6910
ssshyvn <shgood57@gmail.com>
2025-09-21 01:30:49 +0900

    프로젝트 기본 세팅

---
+++ src/main/java/com/dragon/stepbystep/StepByStepBeApplication.java
@@ -0,0 +1,13 @@
+package com.dragon.stepbystep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepByStepBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepByStepBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dragon/stepbystep/StepByStepBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.stepbystep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepByStepBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
