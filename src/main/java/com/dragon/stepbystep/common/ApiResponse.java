43b167ab0248c74ad8f31f679d45e14109bb735e
ssshyvn <shgood57@gmail.com>
2025-10-02 18:42:54 +0900

    Feat: 공통 응답 수정

--- src/main/java/com/dragon/stepbystep/common/ApiResponse.java
+++ src/main/java/com/dragon/stepbystep/common/ApiResponse.java
@@ -10,14 +10,16 @@ import lombok.NoArgsConstructor;
 public class ApiResponse<T> {
     private String status;
     private String message;
+    private String errorCode;
     private T data;
+    private int code;
 
     public static <T> ApiResponse<T> success(String message, T data) {
-        return new ApiResponse<>("success", message, data);
+        return new ApiResponse<>("success", message, null, data, 200);
     }
 
-    public static <T> ApiResponse<T> error(String message) {
-        return new ApiResponse<>("error", message, null);
+    public static <T> ApiResponse<T> error(String errorCode, String message, int code) {
+        return new ApiResponse<>("error", errorCode, errorCode, null, code);
     }
 
 }
