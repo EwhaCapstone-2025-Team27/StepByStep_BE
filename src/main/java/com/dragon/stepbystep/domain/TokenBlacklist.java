beb733e10b55bd701f1ce5f048b0a610b489af0b
ssshyvn <shgood57@gmail.com>
2025-10-02 16:37:43 +0900

    Feat: Auth 엔티티 수정

--- src/main/java/com/dragon/stepbystep/domain/TokenBlacklist.java
+++ src/main/java/com/dragon/stepbystep/domain/TokenBlacklist.java
@@ -17,16 +17,13 @@ import java.time.Instant;
         })
 public class TokenBlacklist {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
-
     @Column(name = "token_hash", nullable = false, length = 128, unique = true)
     private String tokenHash; // Access/Refresh 토큰 해시
-
 
     @Column(name = "expires_at", nullable = false)
     private Instant expiresAt; // 토큰 만료 시각과 동일하게 저장
