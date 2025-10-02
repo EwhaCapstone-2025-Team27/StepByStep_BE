0e54411e046e10ca2d41ffe63f6580cb45306796
ssshyvn <shgood57@gmail.com>
2025-10-02 16:39:30 +0900

    Feat: UserRepository 수정

--- src/main/java/com/dragon/stepbystep/repository/UserRepository.java
+++ src/main/java/com/dragon/stepbystep/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.dragon.stepbystep.repository;
 
 import com.dragon.stepbystep.domain.User;
+import com.dragon.stepbystep.domain.enums.GenderType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,8 +9,12 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
     Optional<User> findByNickname(String nickname);
-    boolean existsByLoginId(String loginId);
     boolean existsByNickname(String nickname);
+
+    // 이메일 찾기(닉네임+성별+출생년도)
+    Optional<User> findByNicknameAndGenderAndBirthyear(String nickname, GenderType gender, Integer birthyear);
 }
