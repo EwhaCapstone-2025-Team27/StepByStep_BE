de2e622493e3ea1f5723cbe6fe0fb0aaf241164f
ssshyvn <shgood57@gmail.com>
2025-10-04 00:11:39 +0900

    Feat: 게시글 엔티티 생성

--- src/main/java/com/dragon/stepbystep/domain/Board.java
+++ src/main/java/com/dragon/stepbystep/domain/Board.java
@@ -1,4 +1,34 @@
 package com.dragon.stepbystep.domain;
 
-public class Board {
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "board_posts")
+public class Board extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User author;
+
+    @Lob
+    @Column(name = "content", nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Builder.Default
+    @Column(name = "comments_count", nullable = false)
+    private int commentsCount = 0;
+
+    @Builder.Default
+    @Column(name = "likes_count", nullable = false)
+    private int likesCount = 0;
 }
