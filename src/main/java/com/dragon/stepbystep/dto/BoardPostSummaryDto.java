8f1862a887dc56ca1617204131c71675cb08f16a
ssshyvn <shgood57@gmail.com>
2025-10-04 00:16:17 +0900

    Feat: Board DTO 생성

---
+++ src/main/java/com/dragon/stepbystep/dto/BoardPostSummaryDto.java
@@ -0,0 +1,29 @@
+package com.dragon.stepbystep.dto;
+
+import com.dragon.stepbystep.domain.Board;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class BoardPostSummaryDto {
+    private final Long postId;
+    private final String nickname;
+    private final LocalDateTime createdAt;
+    private final String content;
+    private final int commentsNum;
+    private final int likesNum;
+
+    public static BoardPostSummaryDto from(Board board) {
+        return BoardPostSummaryDto.builder()
+                .postId(board.getId())
+                .nickname(board.getAuthor().getNickname())
+                .createdAt(board.getCreatedAt())
+                .content(board.getContent())
+                .commentsNum(board.getCommentsCount())
+                .likesNum(board.getLikesCount())
+                .build();
+    }
+}
