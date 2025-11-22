969814797a35a4831bf281b97a4b312adf19668c
ssshyvn <shgood57@gmail.com>
2025-11-23 01:48:59 +0900

    Feat: 뱃지 구매 로직 수정

--- src/main/java/com/dragon/stepbystep/service/BadgeService.java
+++ src/main/java/com/dragon/stepbystep/service/BadgeService.java
@@ -115,6 +115,12 @@ public class BadgeService {
                         .build()
         );
 
+        saveBadgePurchaseHistory(user, badge, price);
+
+        return BadgePurchaseResponseDto.of(userBadge, before, price, user.getPoints());
+    }
+
+    private void saveBadgePurchaseHistory(User user, Badge badge, int price) {
         pointHistoryRepository.save(
                 PointHistory.builder()
                         .user(user)
@@ -124,8 +130,6 @@ public class BadgeService {
                         .balanceAfter(user.getPoints())
                         .build()
         );
-
-        return BadgePurchaseResponseDto.of(userBadge, before, price, user.getPoints());
     }
 
     private int normalizeLimit(Integer limitParam) {
