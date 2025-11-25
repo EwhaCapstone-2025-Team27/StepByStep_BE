a25c2c8980fd01e3182ce3fa9850252ab473f94d
ssshyvn <shgood57@gmail.com>
2025-11-25 17:22:29 +0900

    Feat: 배지 구매 내역

---
+++ src/main/java/com/dragon/stepbystep/domain/BadgePurchaseHistory.java
@@ -0,0 +1,28 @@
+package com.dragon.stepbystep.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "badge_purchase_histories")
+public class BadgePurchaseHistory extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "badge_id", nullable = false)
+    private Badge badge;
+
+    @Column(name = "price_at_purchase", nullable = false)
+    private Integer priceAtPurchase;
+}
